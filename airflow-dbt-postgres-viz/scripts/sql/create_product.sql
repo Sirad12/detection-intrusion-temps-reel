5c017f3458d8ea4781cd1fc4c48fd465fa281cda
Ndeye Sira Dia <siradia167@gmail.com>
2026-05-12 17:52:59 +0000

    Suppression dossiers airflow-dbt-postgres-viz et dbt-postgres

--- airflow-dbt-postgres-viz/scripts/sql/create_product.sql
+++
@@ -1,9 +0,0 @@
--- Table: product
-CREATE TABLE operations.product (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    code VARCHAR(50) UNIQUE,
-    pu_price NUMERIC(10, 2),
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status BOOLEAN DEFAULT TRUE
-);
